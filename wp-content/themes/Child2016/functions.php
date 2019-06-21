2d6c2d08e330dae4d853a51e69b328d11072e210
Kenneth <kennethspapa@gmail.com>
2019-06-21 00:45:02 -0400

    updated styleshet for google fonts

--- wp-content/themes/Child2016/functions.php
+++ wp-content/themes/Child2016/functions.php
@@ -1,7 +1,18 @@
 <?php
+
+function wpb_add_google_fonts() {
+
+wp_enqueue_style( 'wpb-google-fonts',"
+	https://fonts.googleapis.com/
+	css?family=Knewave|Roboto&display=swap", false );
+}
+
+add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' )
+	;
+
 function my_theme_enqueue_styles() {    $parent_style = '
 
-	twentysixteen-style'; // This is 'twentysixteen-style' for the Twenty Fifteen theme.
+	twentysixteen-style'; // This is 'twentysixteen-style' for the Twenty Sixteen theme.
 
 	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 		wp_enqueue_style( 'child-style',get_stylesheet_directory_uri() . '/style.css',       
@@ -10,7 +21,4 @@ function my_theme_enqueue_styles() {    $parent_style = '
 	}
 	add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 	
-
-	function wpb_add_google_fonts() {
-		wp_enqueue_style( 'wpb-google-fonts','https://fonts.googleapis.com/css?family=Encode+Sans+Expanded|Lato|Shadows+Into+Light&display=swap', false ); }add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );note 
 	?>
